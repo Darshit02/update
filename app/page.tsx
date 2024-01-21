fded76d5c1dfa68118553f1909c9b2be380fbb4b
LazY <darshitmistry2@gmail.com>
2024-01-21 11:50:41 +0530

    fix:conflict

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,10 @@
 import Content from '@/components/Content'
-<<<<<<< HEAD
-import Join from '@/components/Join'
-import Navbar from '@/components/Navbar'
-import Services from '@/components/Services'
-=======
 import Footer from '@/components/Footer'
+import Join from '@/components/Join'
 import Navbar from '@/components/Navbar'
 import Services from '@/components/Services'
 import ScrollToTopButton from '@/components/ui/ScrollTop'
 import React from 'react'
->>>>>>> 479c45dc33922e154a0c28dac5a2dc805c87f688
 
 const Page = () => {
   return (
@@ -17,12 +12,9 @@ const Page = () => {
       <Navbar/>
       <Content/>
       <Services/>
-<<<<<<< HEAD
       <Join/>
-=======
       <Footer />
       <ScrollToTopButton />
->>>>>>> 479c45dc33922e154a0c28dac5a2dc805c87f688
     </>
 
     )
